c0d2521f4e5a5500ca04682fc6f23e35b0c72286
Heather Roberts <heather.roberts427@gmail.com>
2017-10-28 18:46:51 +1100

    Randomise position of tiles on board

--- board.js
+++ board.js
@@ -2,23 +2,53 @@ import { Tile } from './tile.js';
 
 export class Board {
   constructor({numRows, numCols, tileSize}) {
+    this.tiles = [];
+
     this.boardContainer = document.createElement('div');
     this.boardContainer.className = 'board-container';
 
-    this.setGridLayout(numRows, numCols);
-    this.generateTiles(numRows * numCols - 1, tileSize);
+    this.initGrid(numRows, numCols, tileSize);
   }
 
-  setGridLayout(numRows, numCols) {
+  initGrid(numRows, numCols, tileSize) {
     this.boardContainer.setAttribute('style', 
-      `grid-template-columns: repeat(${numCols}, 1fr);
-       grid-template-rows: repeat(${numRows}, 1fr);`);
+    `grid-template-columns: repeat(${numCols}, 1fr);
+     grid-template-rows: repeat(${numRows}, 1fr);`);
+
+    const numTiles = numRows * numCols - 1;
+
+    const tileIdxs = Array(numTiles).fill(0).map((el, idx) => idx + 1);
+    this.shuffle(tileIdxs);
+
+    let curRow = 1;
+    let curCol = 1;
+    for (let i = 0; i < numTiles; i++) {
+      if (curCol < numCols) {
+        curCol++;
+      } else {
+        curCol = 1;
+        curRow++;
+      }
+
+      const tile = new Tile({number: tileIdxs[i], size: tileSize});
+      tile.element.setAttribute('style', `
+        grid-column-start: ${curCol};
+        grid-column-end: ${curCol + 1};
+        grid-row-start: ${curRow};
+        grid-row-end: ${curRow + 1};`);
+      this.tiles.push(tile);
+      this.boardContainer.appendChild(tile.element);      
+    }
   }
 
-  generateTiles(numTiles, tileSize) {
-    for (let i = 0; i < numTiles; i++) {
-      const tile = new Tile({number: i, size: tileSize})
-      this.boardContainer.appendChild(tile.element);
+  /**
+   * Taken from https://stackoverflow.com/a/6274381/521531
+   * Shuffles array in place
+   */
+  shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
     }
   }
 
