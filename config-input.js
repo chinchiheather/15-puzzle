a4e4c96c628357348534d4c149634b268dacccb2
Heather Roberts <heather.roberts427@gmail.com>
2017-10-31 14:12:55 +1000

    Prevent user manually entering a number lower than the allowed min value

--- config-input.js
+++ config-input.js
@@ -2,7 +2,7 @@ export class ConfigInput {
   constructor(label, defaultVal, minVal, onInputChange) {
     this.inputContainer = document.createElement('div');
     this.inputContainer.className = 'input-container';
-    
+
     const labelEl = document.createElement('label');
     labelEl.className = 'config-label';
     labelEl.textContent = label;
@@ -12,7 +12,15 @@ export class ConfigInput {
     inputEl.type = 'number';
     inputEl.value = defaultVal;
     inputEl.setAttribute('min', minVal);
-    inputEl.onchange = (event) => onInputChange(+event.target.value);
+    inputEl.onchange = (event) => {
+      let newVal = +event.target.value;
+      // even with min attribute a user can manually enter a number outside the allowed range
+      if (newVal < minVal) {
+        newVal = minVal;
+        inputEl.value = minVal;
+      }
+      onInputChange(newVal);
+    };
     this.inputContainer.appendChild(inputEl);
   }
 
