8e610932ba90ea1b4b2d35de52114b6c7704ba47
Heather Roberts <heather.roberts427@gmail.com>
2017-11-01 11:41:52 +1000

    Create 'Add New Board' button in app.js rather than relying on it being present in the html

--- src/app.js
+++ src/app.js
@@ -3,15 +3,23 @@ import { Game } from './components/game.js';
 (() => {
   function init() {
     addGame();
-
-    const addBoardBtn = document.getElementsByClassName('add-board-btn')[0];
-    addBoardBtn.onclick = addGame;
+    addNewBoardButton();
   }
 
   function addGame() {
     const game = new Game();
-    const puzzleContainer = document.getElementById('15-puzzle');
+    const puzzleContainer = document.querySelector('#fifteen-puzzle .puzzles-container');
     puzzleContainer.appendChild(game.element);
+  }
+
+  function addNewBoardButton() {
+    const addBoardBtn = document.createElement('button');
+    addBoardBtn.className = 'add-board-btn';
+    addBoardBtn.textContent = 'Add New Board';
+    addBoardBtn.addEventListener('click', addGame);
+
+    const buttonContainer = document.querySelector('#fifteen-puzzle .button-container');
+    buttonContainer.appendChild(addBoardBtn);
   }
 
   init();
